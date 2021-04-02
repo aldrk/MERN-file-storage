968de8e4f84ab97c7be79d49c13e33ad69fdc9dc
Alexandr <sashka.krup@gmail.com>
2021-04-02 09:12:55 +0300

    modal done

---
+++ frontend/src/app/sections/Disk/Modal/index.tsx
@@ -0,0 +1,51 @@
+import React, {FC, useState} from "react"
+import {TextField, Modal as ModalWindow, Button} from "@material-ui/core"
+import {Alert} from "@material-ui/lab"
+
+import style from "./style.module.scss"
+import {createDir} from "store/dispatchers/files"
+import {useDispatch, useSelector} from "react-redux"
+import {State} from "store/store"
+
+type Props = {
+  onClose: () => void
+  open: boolean
+}
+
+const Modal: FC<Props> = ({onClose, open}) => {
+  const dispatch = useDispatch()
+  const {currentDir} = useSelector((state: State) => state.files)
+  const [newDirName, setNewDirName] = useState("")
+  const [isError, setError] = useState(false)
+
+  const createDirHandler = () => {
+    if (newDirName.length === 0) {
+      setError(true)
+      return
+    }
+
+    setError(false)
+    dispatch(createDir(currentDir, newDirName))
+    setNewDirName("")
+    onClose()
+  }
+
+  return (
+    <ModalWindow
+      className={style.modal}
+      open={open}
+      onClose={onClose}
+    >
+      <div className={style.modalContent}>
+        <TextField className={style.input} color="primary" value={newDirName} onChange={e => setNewDirName(e.target.value)} />
+        <div className={style.buttonsWrapper}>
+          <Button color="primary" variant="outlined" onClick={onClose}>Закрыть</Button>
+          <Button color="primary" variant="outlined" onClick={createDirHandler}>Создать</Button>
+        </div>
+        {isError && <Alert className={style.alert} severity="warning">Please enter directory name</Alert>}
+      </div>
+    </ModalWindow>
+  )
+}
+
+export default Modal
--- frontend/src/app/sections/Disk/index.tsx
+++ frontend/src/app/sections/Disk/index.tsx
@@ -1,30 +1,29 @@
 import {Box, Button} from "@material-ui/core"
-import React, {useEffect} from "react"
+import React, {useEffect, useState} from "react"
 import {useDispatch, useSelector} from "react-redux"
-import {createDir, getFiles} from "store/dispatchers/files"
+import {getFiles} from "store/dispatchers/files"
 import {State} from "store/store"
 import FilesTable from "./FilesTable"
+import Modal from "./Modal"
 
 import style from "./style.module.scss"
 
 const Disk = () => {
   const dispatch = useDispatch()
   const {currentDir, files} = useSelector((state: State) => state.files)
+  const [isModalOpen, setModalOpen] = useState(false)
 
   useEffect(() => {
     dispatch(getFiles(currentDir))
   }, [currentDir])
 
-  const createDirHandler = () => {
-    dispatch(createDir(currentDir, "juk"))
-  }
-
   return (
     <div>
       <Box className={style.wrapper}>
+        <Modal open={isModalOpen} onClose={() => setModalOpen(false)}/>
         <div className={style.buttons}>
           <Button color="primary" variant="outlined">Назад</Button>
-          <Button color="primary" variant="outlined" onClick={createDirHandler}>Создать папку</Button>
+          <Button color="primary" variant="outlined" onClick={() => setModalOpen(true)}>Создать папку</Button>
         </div>
         <FilesTable files={files} />
       </Box>
